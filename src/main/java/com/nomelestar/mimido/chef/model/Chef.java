5e0248934d772631e056be4b9551d0494e2ac2cc
Sebastian Zapata <pansezapata@gmail.com>
2026-01-26 06:21:13 -0500

    se organizan los datos

--- src/main/java/com/nomelestar/mimido/chef/model/Chef.java
+++ src/main/java/com/nomelestar/mimido/chef/model/Chef.java
@@ -20,7 +20,7 @@ public class Chef {
     @Column(nullable=false)
     private String description;
     @Column(nullable=false, length = 80)
-    private String speciality;
+    private String specialty;
 
     @OneToMany(mappedBy = "chef", cascade = CascadeType.ALL)
     private List<Dish> dishes;
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,8 +4,8 @@ TRUNCATE TABLE chefs RESTART IDENTITY CASCADE;
 
 -- Chefs
 INSERT INTO chefs (name, description,specialty) VALUES
-                                        ('Juan Pérez', "Mandar",'Italiana'),
-                                        ('María Gómez', "Tacos",'Mexicana'),
-                                        ('Carlos Ruiz', "Picana",'Parrilla'),
-                                        ('Ana Torres', "Crembrule",'Pastelería'),
-                                        ('Luisa Fernández', "Otra cosa",'Vegetariana');
+                                        ('Juan Pérez', 'Mandar','Italiana'),
+                                        ('María Gómez', 'Tacos','Mexicana'),
+                                        ('Carlos Ruiz', 'Picana','Parrilla'),
+                                        ('Ana Torres', 'Crembrule','Pastelería'),
+                                        ('Luisa Fernández', 'Otra cosa','Vegetariana');
