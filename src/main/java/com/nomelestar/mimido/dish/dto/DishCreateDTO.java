84fcf9dc52ccb3b2222960d79caa777277400045
Sebastian Zapata <pansezapata@gmail.com>
2026-03-03 11:01:08 -0500

    Se cuadran las pruebas de dish

--- src/main/java/com/nomelestar/mimido/dish/dto/DishCreateDTO.java
+++ src/main/java/com/nomelestar/mimido/dish/dto/DishCreateDTO.java
@@ -6,16 +6,9 @@ import java.math.BigDecimal;
 
 public record DishCreateDTO(
 
-        @NotBlank(message = "please please my darling")
-        @Size(min=2, max = 140)
-         String name,
+                @NotBlank(message = "please please my darling") @Size(min = 2, max = 140) String name,
 
-        @NotBlank(message = "price is required")
-        @DecimalMin(value="0.00",inclusive = false,message = "price must be greater than 0")
-        @Digits(integer = 8,fraction = 2)
-        BigDecimal price,
+                @NotNull(message = "price is required") @DecimalMin(value = "0.00", inclusive = false, message = "price must be greater than 0") @Digits(integer = 8, fraction = 2) BigDecimal price,
 
-        @NotNull(message = "chefId is required")
-        @Positive(message = "chefId has to be positive mf")
-        Long chefId
-) { }
+                @NotNull(message = "chefId is required") @Positive(message = "chefId has to be positive mf") Long chefId) {
+}
